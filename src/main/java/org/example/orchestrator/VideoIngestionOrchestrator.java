f6ec6878fb1a6e25260bc03865e4c1b425fa8f13
Jordan Fields <74157993+Nadr0jj@users.noreply.github.com>
2025-02-15 17:30:19 -0800

    Improve logging

--- src/main/java/org/example/orchestrator/VideoIngestionOrchestrator.java
+++ src/main/java/org/example/orchestrator/VideoIngestionOrchestrator.java
@@ -2,10 +2,12 @@
 package org.example.orchestrator;
 
 import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.ThreadContext;
 import org.example.download.VideoDownloaderTask;
 import org.example.model.VideoDownloaderTaskResult;
 
 import java.nio.file.Path;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -16,12 +18,18 @@ public class VideoIngestionOrchestrator {
 
     public void startIngestion(final String pageUrl) {
         log.info("Starting ingestion.");
+        // Create mongodb record for the job and set the status to IN_PROGRESS
+        final Map<String, String> threadContext = ThreadContext.getContext();
+
         CompletableFuture.supplyAsync(() -> {
             log.info("Starting task completable");
+            ThreadContext.putAll(threadContext);
             final VideoDownloaderTask videoDownloaderTask = new VideoDownloaderTask(pageUrl);
             VideoDownloaderTaskResult result = videoDownloaderTask.call();
             log.info("Finished download task {}", result);
             return result;
         }, executor);
+
+        log.info("Async video downloader task supplied to async thread.");
     }
 }
