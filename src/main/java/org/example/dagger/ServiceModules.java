539b51870c26eb0f3e5fbef994a5f6300f4c32d4
Jordan Fields <74157993+Nadr0jj@users.noreply.github.com>
2025-02-16 17:58:19 -0800

    Add pages as a dependency

--- src/main/java/org/example/dagger/ServiceModules.java
+++ src/main/java/org/example/dagger/ServiceModules.java
@@ -1,11 +1,13 @@
 /* (C)2024 */
 package org.example.dagger;
 
+import com.github.nadr0j.pagesclient.ApiClient;
+import com.github.nadr0j.pagesclient.Configuration;
+import com.github.nadr0j.pagesclient.api.DefaultApi;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dagger.Module;
 import dagger.Provides;
 import org.example.orchestrator.VideoIngestionOrchestrator;
-
 import javax.inject.Singleton;
 
 @Module
@@ -20,5 +22,13 @@ public class ServiceModules {
     @Singleton
     public VideoIngestionOrchestrator provideVideoIngestionOrchestrator() {
         return new VideoIngestionOrchestrator();
+    }
+
+    @Provides
+    @Singleton
+    public DefaultApi providePagesClient() {
+        final ApiClient defaultClient = Configuration.getDefaultApiClient();
+        defaultClient.setBasePath("10.0.0.102");
+        return new DefaultApi(defaultClient);
     }
 }
